6c89f573063939041676c1127c9a9ec9fff640af
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-09 14:41:41 +0200

    Clean up a little

--- model.R
+++ model.R
@@ -7,39 +7,35 @@ library("deSolve")
 growth = function(t, y, params) {
   with(as.list(c(params,y)), {
     ## function values
-    
-    # compute derivatives
-    pa    = (t*b*tarm*zigma)
-    fa    = t*b*tarm*zigma
-    ka    = t*b*tarm*zigma
-    fptz  = p*t*zigma
-    g     = t*P/zigma
-    h     = t*f/zigma*k/zigma*v/zigma
-    i     = t*k/zigma*v/zigma
-    j     = t*H*v/zigma
-    zigma = p+f+k+v
+    pa    = (temp*b*tarm*zigma)
+    fa    = temp*b*tarm*zigma
+    ka    = temp*b*tarm*zigma
+    fptz  = p*temp*zigma
+    g     = temp*P/zigma
+    h     = temp*F/zigma*K/zigma*V/zigma
+    i     = temp*K/zigma*V/zigma
+    j     = temp*H*V/zigma
+    zigma = P+F+K+V
 
     pp    = pi/(pi+fi+ki)
     fp    = fi/(pi+fi+ki)
     kp    = ki/(pi+fi+ki)
-    
 
     ## derivatives
     #1    = pp+fp+kp
     dtarm = fptz -(pi+fi+ki)
-  #dtarm  = 0
-    
+    #dtarm  = 0
     dPx   = pi-pa
     dFx   = fi-fa
     dKx   = ki-ka
-    
+
     dP    = pa*g
     dUx   = g
     dF    = fa-h
-    dk    = ka+h-i
-    dv    = i-j
-    dvx   = j
-    
+    dK    = ka+h-i
+    dV    = i-j
+    dVx   = j
+
     return(list(c(dPx, dFx, dKx, dUx, dVx,
                   dtarm,
                   dP, dF, dK, dV)))
