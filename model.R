ccbac6c9effedc897f57800c131459130f02448a
Wedenborg <emiliewedenborg@gmail.com>
2018-04-09 14:48:26 +0200

    startværdiier

--- model.R
+++ model.R
@@ -1,13 +1,9 @@
 library("deSolve")
 
-
-
-
-=======
 growth = function(t, y, params) {
   with(as.list(c(params,y)), {
     ## function values
-    
+
     # compute derivatives
     pa    = (t*b*tarm*zigma)
     fa    = t*b*tarm*zigma
@@ -22,26 +18,30 @@ growth = function(t, y, params) {
     pp    = pi/(pi+fi+ki)
     fp    = fi/(pi+fi+ki)
     kp    = ki/(pi+fi+ki)
-    
+
 
     ## derivatives
     #1    = pp+fp+kp
     dtarm = fptz -(pi+fi+ki)
   #dtarm  = 0
-    
+
     dPx   = pi-pa
     dFx   = fi-fa
     dKx   = ki-ka
-    
+
     dP    = pa*g
     dUx   = g
     dF    = fa-h
     dk    = ka+h-i
     dv    = i-j
     dvx   = j
-    
+
     return(list(c(dPx, dFx, dKx, dUx, dVx,
                   dtarm,
                   dP, dF, dK, dV)))
   })
 }
+
+initials=c(Px=0, Fx=0, Kx=0, Ux=0, Vx=0,
+           dtarm=0,
+           dP=0, dF=0, dK=0, dV=0)
