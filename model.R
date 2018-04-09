e3ada2042a26b792e785dc486171ed988337aaab
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-09 14:22:20 +0200

    Add skeleton for diff eq system

--- model.R
+++ model.R
@@ -1,1 +1,13 @@
 library("deSolve")
+
+growth = function(t, y, params) {
+  with(as.list(c(params,y)), {
+    ## function values
+
+    ## derivatives
+
+    return(list(c(dPx, dFx, dKx, dUx, dVx,
+                  dtarm,
+                  dP, dF, dK, dV)))
+  })
+}
