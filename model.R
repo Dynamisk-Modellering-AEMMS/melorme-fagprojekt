2877a187dd2c7f1524258c0421c9da040c1f399a
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-16 16:20:00 +0200

    Way more realistic model

--- model.R
+++ model.R
@@ -4,15 +4,14 @@ growth = function(t, y, params) {
   with(as.list(c(params,y)), {
     ## function values
     zigma = P+FF+K+V
-    #f = K/100*(143.23-.144*density)      # food consumed vs.Density pr dm^2
+    f = (zigma/80)*(637.27 - 0.305*density)/12/7 # [density] = 1/(dm^2), (zigma/80) er et gæt for hver meget mere større larver spiser
     print(c(P,FF,K,V))
     print(c(P*100/zigma,FF*100/zigma,K*100/zigma,V*100/zigma))
-    f = max(zigma,0)+1
-    g = log(exp((P-0.46*zigma)/5)+1)*5
+    #f = max(zigma,0)+1
+    g = log(exp((P-0.46*zigma))+1) # "gæt", sørger for maks 46% proteinindhold
     print(g)
     h = 0
-    #i = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
-    i = 0
+    i = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
     j = V/(zigma*H)
 
     pi = pp*f
@@ -43,7 +42,7 @@ growth = function(t, y, params) {
 params = c(
   temp = 20,
   b = 0,
-  density = 10,
+  density = 50,
   H = 0.4,
 
   pp = 0.2,
@@ -53,8 +52,8 @@ params = c(
 
 initials = c(
   Px = 0, Fx = 0, Kx = 0, Ux = 0, Vx = 0,
-  P = 1, FF = 1, K = 1, V = 1
+  P = 2, FF = 3, K = 5, V = 1
 )
 
-sols = ode(initials,c(1:30),growth,params)
+sols = ode(initials,c(1:84),growth,params)
 plot(sols)
