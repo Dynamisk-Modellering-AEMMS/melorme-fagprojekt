0074a6d6061bbd566a169df0177348c4878619a7
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-21 20:59:52 +0200

    Så mange gæt :(

--- model.R
+++ model.R
@@ -5,10 +5,11 @@ growth = function(t, y, params) {
     ## function values
     zigma = P+FF+K+V
     f = (zigma/80)*(637.27 - 0.305*density)/12/7 # [density] = 1/(dm^2), (zigma/80) er et gæt for hver meget mere større larver spiser
-    g = max((P-0.46*zigma),0) # "gæt", sørger for maks 46% proteinindhold
-    h = 0
-    kCombust = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
-    fCombust = 0
+    g = 0.125 * P # "gæt", sørger for maks 46% proteinindhold
+    h = 0.05*K
+    #kCombust = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
+    kCombust = 0.08473955701/0.5*K
+    fCombust = 0.05298229217*FF
     radius = (zigma/(10*pi))^(1/3) # approx radius (as if cylinder of water 5x as long as the diameter)
     j = (0.22 * pi * radius^2) * # surface area
     1440 * # minutes/day
@@ -35,18 +36,19 @@ growth = function(t, y, params) {
     dUx = 0.5*g
     dF = fa + h - fCombust
     dK = ka - h - kCombust
-    dV = 0.5*g + kCombust + fCombust - j
+    #dV = 0.5*g + kCombust + fCombust - j
+    dV = 0.5*g + (0.02108 * zigma) - j # (kCombust + fCombust) = 0.021808 uden skelnen, desværre
     dVx = j
 
     dPl = pp*f - ppl*f - pa
     dFl = fp*f - fpl*f - fa
     dKl = kp*f - kpl*f - ka
-    print(j)
+    #print(j)
 
     return(list(c(dPx, dFx, dKx, dUx, dVx,
                   dP, dF, dK, dV,
                   dPl, dFl, dKl),
-                c(f = f, pa = pa, fa = fa, ka = ka, zigma = zigma, kCombust = kCombust, fCombust = fCombust, j = j, pp = P/zigma, kp = K/zigma, fp = FF/zigma, vp = V/zigma)))
+                c(f = f, pa = pa, fa = fa, ka = ka, zigma = zigma, kCombust = kCombust, fCombust = fCombust, j = j, pp = 100*P/zigma, kp = 100*K/zigma, fp = 100*FF/zigma, vp = 100*V/zigma, ppt = 100*P/(P+K+FF), kpt = 100*K/(P+K+FF), fpt = 100*FF/(P+K+FF), urp = Ux/(Px+Fx+Kx+Ux))))
   })
 }
 
@@ -63,20 +65,20 @@ Pwater = function(temp) { # Antoine ligningen
 }
 
 params = c(
-  temp = 20,
+  temp = 25,
   b = 0,
   density = 50,
-  H = 0.3,
+  H = 0.7,
 
   pressure = 101325, # Pa
   vAir = 0.15, # m/s
-  Dp = 4, # gæt
+  Dp = 35, # gæt
   Df = 4, # gæt
   Dk = 7.2, # (0.015umol/min)/(3 mol/L) * 1 kg/L omregnet til mg/d
 
-  pp = 0.2,
-  fp = 0.4,
-  kp = 0.4
+  pp = 0.33,
+  fp = 0.06,
+  kp = 0.61
 )
 
 initials = c(
@@ -85,5 +87,6 @@ initials = c(
   Pl = 0.2, Fl = 0.3, Kl = 0.5
 )
 
-sols = ode(initials,c(1:84),growth,params)
+sols = ode(initials,c(1:28),growth,params)
 plot(sols)
+print(sols[28,])
