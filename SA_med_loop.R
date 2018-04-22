3b10cdebfc6fa7408bcafce558b22205149362bf
Wedenborg <emiliewedenborg@gmail.com>
2018-04-22 12:10:51 +0200

    Det hele virker!! :smiley:

--- SA_med_loop.R
+++ SA_med_loop.R
@@ -2,81 +2,100 @@ library("deSolve")
 
 growth = function(t, y, params) {
   with(as.list(c(params,y)), {
+
     ## function values
     zigma = P+FF+K+V
-    f = (zigma/80)*(637.27 - 0.305*params[2])/12/7 # [density] = 1/(dm^2), (zigma/80) er et gæt for hver meget mere større larver spiser
-    g = max((P-0.46*zigma),0) # "gæt", sørger for maks 46% proteinindhold
-    h = 0
-    i = -0.3485+0.033054*params[1] #For temp 10-30 grader. Ellers død?
+    f = (zigma/80)*(637.27 - 0.305*density)/12/7 # [density] = 1/(dm^2), (zigma/80) er et gæt for hver meget mere større larver spiser
+    g = 0.125 * P # "gæt", sørger for maks 46% proteinindhold
+    kCombustC = 0.08473955701/0.5
+    h = kCombustC/2*K
+    #kCombust = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
+    kCombust = kCombustC/2*K
+    fCombust = 0.05298229217*FF
     radius = (zigma/(10*pi))^(1/3) # approx radius (as if cylinder of water 5x as long as the diameter)
-    j = 3.4 * #(0.22 * pi * radius^2) * # surface area
+    j = (0.22 * pi * radius^2) * # surface area
       1440 * # minutes/day
       248.33265 * # constant
-      (V/zigma - params[3]) * # humidity difference, diffusion boundary assumed linear, replaces (1-H) in eq
-      params[1]^(-1.4) * sqrt(params[5]) * Pwater(params[1])
+      (V/zigma - H) * # humidity difference, diffusion boundary assumed linear, replaces (1-H) in eq
+      temp^(-1.4) * sqrt(vAir) * Pwater(temp)
 
-    pi = params[8]*f
-    fi = params[9]*f
-    ki = params[10]*f
+    lumenSize = 0.15*zigma
 
-    pa = pi
-    fa = fi
-    ka = ki
+    ppl = Pl/lumenSize
+    fpl = Fl/lumenSize
+    kpl = Kl/lumenSize
+
+    pa = Dp * Pl/lumenSize
+    fa = Df * Fl/lumenSize
+    ka = Dk * Kl/lumenSize # / lumenSize [mg]
 
     ## derivatives
-    dPx = pi-pa
-    dFx = fi-fa
-    dKx = ki-ka
+    dPx = ppl*f
+    dFx = fpl*f
+    dKx = kpl*f
 
     dP = pa-g
     dUx = 0.5*g
-    dF = fa-h
-    dK = ka+h-i
-    dV = 0.5*g + i - j
+    dF = fa + h - fCombust
+    dK = ka - h - kCombust
+    #dV = 0.5*g + kCombust + fCombust - j
+    dV = 0.5*g + (0.02108 * zigma) - j # (kCombust + fCombust) = 0.021808 uden skelnen, desværre
     dVx = j
 
+    dPl = pp*f - ppl*f - pa
+    dFl = fp*f - fpl*f - fa
+    dKl = kp*f - kpl*f - ka
+    #print(j)
+
     return(list(c(dPx, dFx, dKx, dUx, dVx,
-                  dP, dF, dK, dV)))
+                  dP, dF, dK, dV,
+                  dPl, dFl, dKl, f, pa+fa+ka),
+                c(f = f, pa = pa, fa = fa, ka = ka, zigma = zigma, kCombust = kCombust, fCombust = fCombust, j = j, pp = 100*P/zigma, kp = 100*K/zigma, fp = 100*FF/zigma, vp = 100*V/zigma, ppt = 100*P/(P+K+FF), kpt = 100*K/(P+K+FF), fpt = 100*FF/(P+K+FF), urp = Ux/(Px+Fx+Kx+Ux), growthrate=dK+dF+dK)))
   })
 }
 
-Pwater = function(params) { # Antoine ligningen
+Pwater = function(temp) { # Antoine ligningen
   10^(
     2.124903 # for at omregne til Pascal
     + 8.07131 # A
     - (
       1730.63/ # B
         (233.426 # C
-         + params[1]) # [temp] = °C
+         + temp) # [temp] = °C
     )
   )
 }
 
 params = c(
-  temp = 20,
+  temp = 28,
   density = 50,
-  H = 0.3,
+  H = 0.65,
 
   pressure = 101325, # Pa
   vAir = 0.15, # m/s
-  Dp = 1,
-  Df = 1,
+  Dp = 35, # gæt
+  Df = 4, # gæt
+  Dk = 7.2, # (0.015umol/min)/(3 mol/L) * 1 kg/L omregnet til mg/d
 
-  pp = 0.2,
-  fp = 0.4,
-  kp = 0.4
+  pp = 0.33,
+  fp = 0.06,
+  kp = 0.61
 )
 
 initials = c(
   Px = 0, Fx = 0, Kx = 0, Ux = 0, Vx = 0,
-  P = 2, FF = 3, K = 5, V = 1
+  P = 2, FF = 3, K = 5, V = 5,
+  Pl = 0.2, Fl = 0.3, Kl = 0.5,
+  FoodConsumed = 0, FoodDigested = 0
 )
+
 sols = ode(initials,c(1:84),growth,params)
 plot(sols)
+print(sols[84,])
 
 # Scatterplot
-mi = c(10,10,0,10,10,10,10,10,0,0,0)
-ma = c(100,100,1,100,100,100,100,100,1,1,1)
+mi = c(0,10,0,101325/2,0.001,1,1,1,0,0,0)
+ma = c(100,100,1,101325*2,10,100,100,100,1,1,1)
 attribute.names = c('temp','density','H','pressure','vAir','Dp','Df','Dk','pp','fp','kp')
 N.iter <- 12
 P.max <- matrix(data = NA,nrow = length(params),ncol = N.iter)
@@ -96,6 +115,7 @@ for (j in 1:length(params)){
   for ( i in 1:N.iter){
     # simulate the epidemic
     parms[j] = param.list[j,i]
+    names(parms)<-attribute.names
     output <- ode(initials,times = c(1:84), growth, parms)
     # antal syge p� samme tid
     P.max[j,i] <- max(output[,'P'])
@@ -104,7 +124,8 @@ for (j in 1:length(params)){
     # totale antal syge
     K.max[j,i] <- max(output[,'K'])
   }
-  GSA.res <- data.frame(parm1=param.list[j,],P.max[j,],FF.max[j,],K.max[j,])
+  GSA.res <- data.frame(param.list[j,],P.max[j,],FF.max[j,],K.max[j,])
+  names(GSA.res) <- c(attribute.names[j],"P max","F max", "K max")
 
   pairs(GSA.res)
 }
