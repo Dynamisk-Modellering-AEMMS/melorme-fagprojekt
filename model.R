d8f83b255a7ffe6674843995fe866024e00a9603
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-22 11:35:22 +0200

    Hopefully final changes

--- model.R
+++ model.R
@@ -6,9 +6,10 @@ growth = function(t, y, params) {
     zigma = P+FF+K+V
     f = (zigma/80)*(637.27 - 0.305*density)/12/7 # [density] = 1/(dm^2), (zigma/80) er et gæt for hver meget mere større larver spiser
     g = 0.125 * P # "gæt", sørger for maks 46% proteinindhold
-    h = 0.05*K
+    kCombustC = 0.08473955701/0.5
+    h = kCombustC/2*K
     #kCombust = -0.3485+0.033054*temp #For temp 10-30 grader. Ellers død?
-    kCombust = 0.08473955701/0.5*K
+    kCombust = kCombustC/2*K
     fCombust = 0.05298229217*FF
     radius = (zigma/(10*pi))^(1/3) # approx radius (as if cylinder of water 5x as long as the diameter)
     j = (0.22 * pi * radius^2) * # surface area
@@ -47,7 +48,7 @@ growth = function(t, y, params) {
 
     return(list(c(dPx, dFx, dKx, dUx, dVx,
                   dP, dF, dK, dV,
-                  dPl, dFl, dKl),
+                  dPl, dFl, dKl, f, pa+fa+ka),
                 c(f = f, pa = pa, fa = fa, ka = ka, zigma = zigma, kCombust = kCombust, fCombust = fCombust, j = j, pp = 100*P/zigma, kp = 100*K/zigma, fp = 100*FF/zigma, vp = 100*V/zigma, ppt = 100*P/(P+K+FF), kpt = 100*K/(P+K+FF), fpt = 100*FF/(P+K+FF), urp = Ux/(Px+Fx+Kx+Ux), growthrate=dK+dF+dK)))
   })
 }
@@ -65,10 +66,10 @@ Pwater = function(temp) { # Antoine ligningen
 }
 
 params = c(
-  temp = 25,
+  temp = 28,
   b = 0,
   density = 50,
-  H = 0.7,
+  H = 0.65,
 
   pressure = 101325, # Pa
   vAir = 0.15, # m/s
@@ -84,7 +85,8 @@ params = c(
 initials = c(
   Px = 0, Fx = 0, Kx = 0, Ux = 0, Vx = 0,
   P = 2, FF = 3, K = 5, V = 5,
-  Pl = 0.2, Fl = 0.3, Kl = 0.5
+  Pl = 0.2, Fl = 0.3, Kl = 0.5,
+  FoodConsumed = 0, FoodDigested = 0
 )
 
 sols = ode(initials,c(1:84),growth,params)
